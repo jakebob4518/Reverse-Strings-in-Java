a94fadc6b4027beb9ead9d66c11810ac19e9136d
JagTheFriend <jagthefriend12@gmail.com>
2021-06-12 20:31:55 +0200

    Formatted file

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,11 @@
 public class Main {
-
     public static void main(String[] args){
-
-        String input = "wassup everyone";
-        char[] letters = input.toCharArray();
+        String input_string = "hello gamer";
+        char[] letters = input_string.toCharArray();
         int length = letters.length;
-
-        for (int i = length - 1; i >= 0; i-- ){
-
+        
+        for (int i = length - 1; i >= 0; i--){
             System.out.print(letters[i]);
-
         }
     }
 }
