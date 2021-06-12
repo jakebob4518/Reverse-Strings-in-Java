bc0cf2feee9e8e1af495e7287c0c5004bb3c5342
jakebob4518 <74426060+jakebob4518@users.noreply.github.com>
2021-06-12 21:05:48 +0300

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        String input = "wassup everyone";
+        char[] letters = input.toCharArray();
+        int length = letters.length;
+
+        for (int i = length - 1; i >= 0; i-- ){
+
+            System.out.print(letters[i]);
+
+        }
+    }
+}
