47db65ff1370245fcb82ebace102f7a4bdbc52c8
jakebob4518 <74426060+jakebob4518@users.noreply.github.com>
2021-06-13 12:36:20 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args){
+        
         String input_string = "hello gamer";
         char[] letters = input_string.toCharArray();
+        
         int length = letters.length;
         
         for (int i = length - 1; i >= 0; i--){
